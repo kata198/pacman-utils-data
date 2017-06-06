86c1b26855c5797fdc9766d6d485a62c091febbe
Tim Savannah <kata198@gmail.com>
2017-06-05 22:12:01 -0400

    Add install_data.sh

---
+++ install_data.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Installs all the pacman-utils
+
+PROVIDESDB_DIR="/var/lib/pacman/.providesDB"
+
+for arg in "$@";
+do
+    if ( echo "${arg}" | grep -q '^PREFIX=' );
+    then
+        export "${arg}"
+    elif ( echo "${arg}" | grep -q '^DESTDIR=' );
+    then
+        export "${arg}"
+    fi
+done
+
+if [ -z "${PREFIX}" ];
+then
+    PREFIX="usr"
+else
+    if [ "${PREFIX}" != "/" ];
+    then
+        PREFIX="$(echo "${PREFIX}" | sed 's|[/][/]*$||g')"
+        PREFIX="$(echo "${PREFIX}" | sed 's|//|/|g')"
+    fi
+fi
+if [ -z "${DESTDIR}" ];
+then
+    DESTDIR=""
+else
+    if [ "${DESTDIR}" != "/" ];
+    then
+        DESTDIR="$(echo "${DESTDIR}" | sed 's|[/][/]*$||g')"
+        DESTDIR="$(echo "${DESTDIR}" | sed 's|//|/|g')"
+    else
+        DESTDIR=''
+    fi
+fi
+
+VARDIR="${DESTDIR}/var/lib/pacman"
+VARDIR="$(echo "${VARDIR}" | sed 's|//|/|g')"
+
+mkdir -p "${VARDIR}"
+
+install -v -m 644 "./providesDB" "${VARDIR}/.providesDB"
+RET=$?
+if [ $RET -ne 0 ];
+then
+    echo;
+    echo;
+    echo 'Error installing data! Please check error messages above for more info!'
+    exit $RET
+fi
+
